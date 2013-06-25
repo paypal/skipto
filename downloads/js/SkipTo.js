23df70e25aebf055c6f1e34868538203ba2ee73f
Nawaz <mpnkhan@gmail.com>
2013-06-25 17:13:24 +0530

    Sync with Master

--- downloads/js/SkipTo.js
+++ downloads/js/SkipTo.js
@@ -228,36 +228,8 @@ if (!Window.prototype.addEventListener){
 	  this.returnValue = false;
 	};
 }
- // http://ajaxian.com/archives/creating-a-queryselector-for-ie-that-runs-at-native-speed
 
-  if (!document.querySelectorAll) {
-	document.querySelectorAll = function (selectors) {
-	  var style = document.createElement('style'), elements = [], element;
-	  document.documentElement.firstChild.appendChild(style);
-	  document._qsa = [];
-
-	  style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-	  window.scrollBy(0, 0);
-	  style.parentNode.removeChild(style);
-
-	  while (document._qsa.length) {
-		element = document._qsa.shift();
-		element.style.removeAttribute('x-qsa');
-		elements.push(element);
-	  }
-	  document._qsa = null;
-	  return elements;
-	};
-  }
-
-  if (!document.querySelector) {
-	document.querySelector = function (selectors) {
-	  var elements = document.querySelectorAll(selectors);
-	  return (elements.length) ? elements[0] : null;
-	};
-  }
-
-  if (!document.getElementsByClassName) {
+if (!document.getElementsByClassName) {
 	document.getElementsByClassName = function (classNames) {
 		classNames = String(classNames).replace(/^|\s+/g, '.');
 		return document.querySelectorAll(classNames);
