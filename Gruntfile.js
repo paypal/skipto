e31e7abb41c7b6bedb0b42e9e10ef3543dfa7600
Kim Doberstein (UMN) <oit-dobe0002@x-128-101-23-9.dhcp.umn.edu>
2017-01-10 11:29:03 -0600

    Modified gruntfile.js file it fix a defect in the minified versions of the compiled files
    The ordering of the complied javascript should be shimIE, dropDownMenu,
    skipTo.
    
    While this ordering is correct for the skipto.js file, the ordering was
    incorrect for the skipto.min.js file.  This commit fixes that defect.

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
 			},
 			dist: {
 				files: {
-					'./compiled/js/<%= pkg.name %>.min.js': ['src/js/shimIE.js', 'src/js/<%= pkg.name %>.js', 'src/js/dropMenu.js']
+					'./compiled/js/<%= pkg.name %>.min.js': ['src/js/shimIE.js', 'src/js/dropMenu.js', 'src/js/<%= pkg.name %>.js']
 				}
 			}
 		},
