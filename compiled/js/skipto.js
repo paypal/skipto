1f56a53f1e9806d0d283b005dca058ad3d04497a
Jon Gunderson <jongund@illinois.edu>
2021-09-17 09:21:28 -0500

    updated compiled files

--- compiled/js/skipto.js
+++ compiled/js/skipto.js
@@ -1,4 +1,4 @@
-/*! skipto - v4.1.2 - 2021-09-14
+/*! skipto - v4.1.2 - 2021-09-17
 * https://github.com/paypal/skipto
 * Copyright (c) 2021 PayPal Accessibility Team and University of Illinois; Licensed BSD */
  /*@cc_on @*/
