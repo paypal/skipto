2c0f3f0eba1117d40c6d296c9065b022f89b0cc3
Jon Gunderson <jongund@illinois.edu>
2021-09-17 09:23:11 -0500

    updated documentation

--- src/js/skipto.js
+++ src/js/skipto.js
@@ -1,5 +1,5 @@
 /* ========================================================================
-* Copyright (c) <2021> PayPal and University of Illinois
+* Copyright (c) <2021> PayPal and University of Illinois at Urbana Champgain
 * All rights reserved.
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
