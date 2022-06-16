75b3d7bc6804c7d131e1668351f9c090e4a05b8c
Jon Gunderson <jongund@illinois.edu>
2022-06-16 13:57:54 -0500

    updated style id

--- downloads/js/skipto.js
+++ downloads/js/skipto.js
@@ -18,8 +18,8 @@
 (function() {
   'use strict';
   const SkipTo = {
-    skipToId: 'id-skip-to-js-5',
-    skipToMenuId: 'id-skip-to-menu-5',
+    skipToId: 'id-skip-to-js-42',
+    skipToMenuId: 'id-skip-to-menu-42',
     domNode: null,
     buttonNode: null,
     menuNode: null,
